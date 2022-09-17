274e24fc212bd0bee4edbaa41a4a299f941c6b93
David Justice <davidjustice28@gmail.com>
2022-09-16 22:27:59 -0400

    recommit image fixes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+
 function zoom(id,scale) {
     document.getElementById(id).style.transform = `scale(${scale})`
-    
 }
 setTimeout(() => {
-    zoom("main-logo", 1)
+    document.getElementById("main-logo").style.transform = "scale(1)"
     setTimeout(() => {
         document.querySelector("h3").style.opacity = "1"
     }, 1000);
 }, 500)
 
 const h1 = document.querySelector("h1")
+
 
 window.onscroll = function loadheader() {
     if(document.documentElement.scrollTop> 75) {
@@ -19,21 +20,17 @@ window.onscroll = function loadheader() {
     }
 }
 
-if(document.getElementById("screenshot-div").style.width = "100vw") {
-    return
-}else {
 
 
 const div1 = document.getElementById("info1")
-div1.addEventListener("mouseover", () => zoom("example1",1.5 ))
-div1.addEventListener("mouseout", () => zoom("example1", .7))
+div1.addEventListener("mouseover", () => zoom("example1",1 ))
+div1.addEventListener("mouseout", () => zoom("example1", .5))
 const div2 = document.getElementById("info2")
-div2.addEventListener("mouseover", () => zoom("example2", 1.5))
-div2.addEventListener("mouseout", () => zoom("example2", .7))
+div2.addEventListener("mouseover", () => zoom("example2", 1))
+div2.addEventListener("mouseout", () => zoom("example2", .5))
 const div3 = document.getElementById("info3")
 div3.addEventListener("mouseover", () => zoom("example3",1 ))
-div3.addEventListener("mouseout", () => zoom("example3", .7))
+div3.addEventListener("mouseout", () => zoom("example3", .5))
 const div4 = document.getElementById("info4")
 div4.addEventListener("mouseover", () => zoom("example4",1 ))
-div4.addEventListener("mouseout", () => zoom("example4", .7))
-}
+div4.addEventListener("mouseout", () => zoom("example4", .5))
